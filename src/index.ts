0d63e5848048b79e668013b3cd7860c99b5ef75d
phcsilva056 <phc.silva@outlook.com.br>
2022-09-11 20:47:00 -0300

    nova endpoint de turma, seleciona as turmas novas com modulo 0

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import { app } from './app'
-import { createTeacher, getTeachers, updateTeacher } from './endpoints/teachers'
-import { getClass, postClass, putClass } from './endpoints/class'
-import { createStudent, getStudantByName } from './endpoints/students'
+import { app } from "./app";
+import {
+  createTeacher,
+  getTeachers,
+  updateTeacher,
+} from "./endpoints/teachers";
+import { getClass, getNewClass, postClass, putClass } from "./endpoints/class";
+import { createStudent, getStudantByName } from "./endpoints/students";
 
-app.post('/teacher', createTeacher)
-app.get('/teacher', getTeachers)
-app.put('/teacher', updateTeacher)
+app.post("/teacher", createTeacher);
+app.get("/teacher", getTeachers);
+app.put("/teacher", updateTeacher);
 
-app.post('/class', postClass)
-app.get('/class', getClass)
-app.put('/class/:id', putClass)
+app.post("/class", postClass);
+app.get("/class", getClass);
+app.get("/class/new", getNewClass);
+app.put("/class/:id", putClass);
 
-app.post('/student', createStudent)
-app.get('/student/:name', getStudantByName)
+app.post("/student", createStudent);
+app.get("/student/:name", getStudantByName);
